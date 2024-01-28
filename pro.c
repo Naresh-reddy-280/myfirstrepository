aadb50e3b4f6b852e3b5b8adaf2578e2c21f6867
Naresh <nareshchandragiri280@gmail.com>
2024-01-28 21:59:45 +0530

    adding a function

--- pro.c
+++ pro.c
@@ -5,5 +5,9 @@ int main()
     cout<<"this is my first program"<<endl;
     int a=10;
 	cout<<a<<endl;
-	cout<<a<<endl;
+	// cout<<a<<endl;
+	void fun()
+	{
+		cout<<"This is a function"<<endl;
+	}
 }
